1be01d5f68b32a57b5f5b84945211286ef2e98c3
xiaofan0408 <1002758393@qq.com>
2022-05-10 18:37:14 +0800

    初始化代码

---
+++ src/main/java/com/xiaofan0408/App.java
@@ -0,0 +1,13 @@
+package com.xiaofan0408;
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception {
+    }
+}
---
+++ src/test/java/com/xiaofan0408/TestApp.java
@@ -0,0 +1,13 @@
+package com.xiaofan0408;
+
+
+/**
+ * @author zefan.xzf
+ * @date 2022/1/26 14:54
+ */
+public class TestApp {
+
+
+
+
+}
